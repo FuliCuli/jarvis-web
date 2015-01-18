87a53787409b281a78637899ab1e420aeda0637e
xherriot <xherriot@gmail.com>
2015-01-18 17:14:02 +0000

    feat(*): general enhancement

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
 
     wiredep: {
       target: {
-        src: '<%= project.dist %>/index.html'
+        src: '<%= project.app %>/index.html'
       }
     },
 
